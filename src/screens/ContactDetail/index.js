13a6ecb397df9e94b1a07b7fc0321dda1a570775
Minwoo Jung <minwoojung01@gmail.com>
2022-02-20 12:57:03 -0700

    Added header options

--- src/screens/ContactDetail/index.js
+++ src/screens/ContactDetail/index.js
@@ -1,10 +1,53 @@
-import {useRoute} from '@react-navigation/native';
-import React from 'react';
+import {useRoute, useNavigation} from '@react-navigation/native';
+import React, {useEffect} from 'react';
 import ContactDetailsComponent from '@components/ContactDetailsComponent';
+import {Text, TouchableOpacity, View} from 'react-native';
+import Icon from '@components/common/Icon';
+import colors from '@assets/theme/colors';
 
 export default function ContactDetail() {
   // 옵셔널 체이닝과 같이 undefined 에러를 방지하는 방법
   const {params: {item = {}} = {}} = useRoute();
+
+  const {setOptions} = useNavigation();
+
+  useEffect(() => {
+    if (item) {
+      setOptions({
+        title: item.first_name + ' ' + item.last_name,
+        headerRight: () => {
+          return (
+            <View style={{flexDirection: 'row', paddingRight: 10}}>
+              <TouchableOpacity>
+                <Icon
+                  size={21}
+                  color={colors.grey}
+                  name={item.is_favorite ? 'start' : 'star-border'}
+                  type="material"
+                />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{paddingLeft: 10}}>
+                <Icon
+                  size={21}
+                  color={colors.grey}
+                  name="delete"
+                  type="material"
+                />
+              </TouchableOpacity>
+            </View>
+          );
+        },
+        headerLeft: () => {
+          return (
+            <View>
+              <Text>Hello</Text>
+            </View>
+          );
+        },
+      });
+    }
+  }, [item]);
 
   return <ContactDetailsComponent contact={item} />;
 }
